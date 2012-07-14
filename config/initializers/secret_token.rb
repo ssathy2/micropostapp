df57b33d6ec665e1241279b993edd69bf97b84a9
Siddharth Sathyam <ssathy2@gmail.com>
2012-07-14 18:30:04 -0500

    Initial Commit of play micropost app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micropostapp::Application.config.secret_token = '4496f6970596f6b25e5157d9418364ebe77ed47ba421074a36276e9524fa1a0f2f7c5ffdf989f814ec6764feee8980decb39b2681751ef7aa1dc24be1cf153b8'
