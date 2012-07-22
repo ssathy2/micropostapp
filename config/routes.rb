d433ed23a6aabca0251a08acb4ec6399304a4460
Siddharth Sathyam <ssathy2@gmail.com>
2012-07-22 11:26:58 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,14 @@ Micropostapp::Application.routes.draw do
 
   resources :users
 
-  root to: 'static_pages#home'
-  
+  root to: 'static_pages#home'  
   match '/signup', to: 'users#new'
 
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
